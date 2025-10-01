5b0a09144f84f1db02e28480f8a5db209bf639f7
Jackson Melzer <jacksonmelzer@Jacksons-MacBook-Air-92.local>
2025-10-01 17:32:58 -0400

    did first part

--- PollReader.py
+++ PollReader.py
@@ -55,16 +55,16 @@ class PollReader():
         """
 
         # iterate through each row of the data
-        for i in self.raw_data:
+        for i in self.raw_data[1:]:
 
             # split up the row by column
-            seperated = i.split(' ')
+            seperated = i.strip().split(',')
 
             # map each part of the row to the correct column
             self.data_dict['month'].append(seperated[0])
             self.data_dict['date'].append(int(seperated[1]))
-            self.data_dict['sample'].append(int(seperated[2]))
-            self.data_dict['sample type'].append(seperated[2])
+            self.data_dict['sample'].append(int(seperated[2].split(" ")[0]))
+            self.data_dict['sample type'].append(seperated[2].split(" "))
             self.data_dict['Harris result'].append(float(seperated[3]))
             self.data_dict['Trump result'].append(float(seperated[4]))
 
@@ -80,7 +80,15 @@ class PollReader():
             str: A string indicating the candidate with the highest polling percentage or EVEN,
              and the highest polling percentage.
         """
-        pass
+        max_harris = max(self.data_dict('Harris result'))
+        max_trump = max(self.data_dict['Trump result'])
+
+        if max_harris > max_trump:
+            return f"Harris with {(max_harris * 100):.1f}"
+        elif max_trump > max_harris:
+            return f"Trump with {(max_trump * 100):.1f}"
+        else:
+            return f"Even with {(max_harris * 100):.1f}"
 
 
     def likely_voter_polling_average(self):
